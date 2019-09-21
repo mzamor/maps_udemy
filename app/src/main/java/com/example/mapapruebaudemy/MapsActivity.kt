b981478b9e016ecf8639553a4bab9056921f75e7
madaza <zamoramariano@hotmail.com>
2019-09-21 20:50:25 -0300

    location permissions

--- app/src/main/java/com/example/mapapruebaudemy/MapsActivity.kt
+++ app/src/main/java/com/example/mapapruebaudemy/MapsActivity.kt
@@ -1,7 +1,11 @@
 package com.example.mapapruebaudemy
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -12,25 +16,21 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
+    private val coarseLocationPermission = android.Manifest.permission.ACCESS_COARSE_LOCATION
+    private val fineLocationPermission = android.Manifest.permission.ACCESS_FINE_LOCATION
+    private val QUERY_PERMISSION_CODE = 100
+
+    var fusedLocationProviderClient : FusedLocationProviderClient? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
@@ -39,4 +39,52 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
+
+    private fun isLocationPermissionValidated(): Boolean {
+        val isFineLocationPermission = ActivityCompat.checkSelfPermission(this,fineLocationPermission) == PackageManager.PERMISSION_GRANTED
+        val isCoarseLocationPermision = ActivityCompat.checkSelfPermission(this, coarseLocationPermission) == PackageManager.PERMISSION_GRANTED
+        return isFineLocationPermission && isCoarseLocationPermision
+    }
+    private fun askForLocation(){
+        val rational = ActivityCompat.shouldShowRequestPermissionRationale(this, fineLocationPermission)
+
+        if (rational){
+            askForLocationPermission()
+        }else{
+            askForLocationPermission()
+        }
+    }
+
+    private fun askForLocationPermission(){
+        requestPermissions(arrayOf(coarseLocationPermission,fineLocationPermission),QUERY_PERMISSION_CODE)
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            QUERY_PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults.get(0) == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                    Toast.makeText(
+                        this,
+                        "No diste permiso para acceder a la ubicacion",
+                        Toast.LENGTH_LONG
+                    )
+                }
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(isLocationPermissionValidated()){
+            askForLocation()
+        }else{
+            askForLocationPermission()
+        }
+    }
+
 }
